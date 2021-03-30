4879955b071a760f0a073b38adbb34a47e7a39d4
Pallavi Joshi <pallavijoshi30oct@gmail.com>
2021-03-30 22:09:11 +0530

    mamba

---
+++ Student/Student.py
@@ -0,0 +1,9 @@
+class Student:
+    name="pal"
+    email="pallavijoshi30oct@gmail.com"
+    usn="1jb17cs096"
+    college="sjbit"
+    def display(self):
+     print(self.name,self.email,self.usn,self.college)
+s=Student()
+s.display()
