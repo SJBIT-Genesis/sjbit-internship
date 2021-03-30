1e48f61468a80ed7be3466a1ad5461f3b318fb8d
Akriti Gupta <akritiabhi03@gmail.com>
2021-03-30 12:05:46 +0530

    student save

---
+++ Student_detials/Student.py
@@ -0,0 +1,12 @@
+
+
+class Student:
+
+    name="Akriti Gupta"
+    email="akritiabhi03@gmail.com"
+    usn="1JB17CS009"
+    phone=8697583238
+
+    def display(self):
+        print(self.name,self.email,self.usn,self.phone)
+
