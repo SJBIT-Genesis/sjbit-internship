c22dcad3afd4a70429ac23989c82d5e89cc0131d
mvasavi42 <mvasavi42@gmail.com>
2021-03-30 21:30:54 +0530

    message

---
+++ Student_Details/Student.py
@@ -0,0 +1,14 @@
+class Student:
+
+    name = "mva"
+    email = "mvasavi42@gmail.com"
+    usn = "1JB17CS172"
+    phone = 9880500676
+
+    def display(self):
+        print(self.name, self.email, self.usn, self.phone)
+
+
+s = Student()
+s.display()
+
