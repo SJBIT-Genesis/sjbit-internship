7871849a420693de909b23bb3e1fb901f9afd40b
Parikshith Sharma <parik1999@live.com>
2021-03-30 21:50:17 +0530

    fixed errors

--- StudentDetails/StudentDetails.py
+++ StudentDetails/StudentDetails.py
@@ -1,12 +1,12 @@
 class StudentDetails:
-    def __init__(self,n,e,c,p):
+    def __init__(self, n, e, c, p):
         self.name = n
         self.email = e
         self.college = c
         self.phno = p
 
     def print_details(self):
-        print('Name:'self.name)
-        print('Email:'self.email)
-        print('College:'self.college)
-        print('Ph No:'self.phno)
+        print('Name:', self.name)
+        print('Email:', self.email)
+        print('College:', self.college)
+        print('Ph No:', self.phno)
