0b79526904a0ed38df3681e8ca508ea74fbffb7c
Preethi B <preethibasavaraju@gmail.com>
2021-03-30 21:43:53 +0530

    Preethi

---
+++ Student_details/StudentDetails.py
@@ -0,0 +1,11 @@
+class StudentDetails:
+    def __int__(self, name, email, usn, college):
+        self.name = name
+        self.email = email
+        self.usn = usn
+        self.college = college
+
+    def my_func(self):
+        print(self.name, self.email, self.usn, self.college)
+
+
