333e714648f4135137366cdb1b5212e6955bbbfa
Megana Bhujang <megbhujang@gmail.com>
2021-03-30 20:26:35 +0530

    Adding Student file

---
+++ student_details/StudentDetails.py
@@ -0,0 +1,9 @@
+class StudentDetails:
+    def __init__(self, name, email, usn, college):
+        self.name = name
+        self.email = email
+        self.usn = usn
+        self.college = college
+
+    def my_func(self):
+        print(self.name, self.email, self.usn, self.college)
