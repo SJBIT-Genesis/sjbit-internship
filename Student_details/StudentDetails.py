bc05e7e29c8da09680fb6b63a4e7798dff24d833
nivedithakirthi <niveditha.kirthi@gmail.com>
2021-03-30 22:44:08 +0530

    committing student details

---
+++ Student_details/StudentDetails.py
@@ -0,0 +1,12 @@
+class StudentDetails:
+    def __int__(self, name, email, usn, college):
+        self.name = name
+        self.email = email
+        self.usn = usn
+        self.college = college
+
+    def print_function(self):
+        print(self.name)
+        print(self.email)
+        print(self.usn)
+        print(self.college)
