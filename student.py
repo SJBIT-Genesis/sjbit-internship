0d86b8c045a8bcb608dffb6c403a266866b96969
Snehadevaiah <snehad0254@gmail.com>
2021-03-31 09:01:14 +0530

    adding studentdetails

---
+++ student.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,name,phno,usn,email):
+        self.name = name
+        self.phno = phno
+        self.usn = usn
+        self.email = email
+
+
+a = Student('Sneha',7259932953,'1JB17CS153','snehad0254@gmail.com')
+print(a.name,a.phno,a.usn,a.email)
+
