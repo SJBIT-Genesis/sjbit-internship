6ff6f0ef18bd797e3481e5a674dd1481dee0c44f
Rheagopal3 <rhea.gopal3@gmail.com>
2021-03-31 09:40:06 +0530

    hello

---
+++ student_details/students.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self, name, phno, usn, email):
+        self.name = name
+        self.phno = phno
+        self.usn = usn
+        self.email = email
+a = student('rhea', 9606187526, '1JB17CS123', 'rhea.gopal3@gmail.com')
+print(a.name, a.phno, a.usn, a.email)
+
+
+
