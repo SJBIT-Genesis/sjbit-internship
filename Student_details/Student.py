5c041cb94b562d463e77eadf4b8a7ea7201ac4e8
trupti-jay <truptikkrishnamurthy@gmail.com>
2021-03-31 10:04:16 +0530

    hello

---
+++ Student_details/Student.py
@@ -0,0 +1,12 @@
+class Student:
+    name="Trupti"
+    email="truptikkrishnamurthy@gmail.com"
+    usn="1JB17CS164"
+    phone=7624804891
+
+
+    def display(self):
+        print(self.name,self.email,self.usn,self.phone)
+
+s=Student()
+s.display()
