00f711b8e1573480d2359a4cb727a393bdba1dd8
Parikshith Sharma <59131456+parik1999@users.noreply.github.com>
2021-03-31 09:45:19 +0530

    Update student.py

--- pg/student.py
+++ pg/student.py
@@ -1,11 +1,12 @@
-class Student:
-    name = ""
-    email = ""
-    usn = ""
-    college =""
+class StudentDetails:
+    def __init__(self, n, e, c, p):
+        self.name = n
+        self.email = e
+        self.college = c
+        self.phno = p
 
-    def getDetails(self):
-        pass
-
-    def setDetails(self):
-        pass
+    def print_details(self):
+        print('Name:', self.name)
+        print('Email:', self.email)
+        print('College:', self.college)
+        print('Ph No:', self.phno)
